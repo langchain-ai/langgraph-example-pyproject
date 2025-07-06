a082eaaf8b1dc1ba00454929831a28bad631af0f
Librascale83 <nate@taile.dev>
2025-07-05 19:19:43 -0500

    Update tools.py

--- my_agent/utils/tools.py
+++ my_agent/utils/tools.py
@@ -1,3 +1,3 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
+# from langchain_community.tools.tavily_search import TavilySearchResults
 
-tools = [TavilySearchResults(max_results=1)]
+# tools = [TavilySearchResults(max_results=1)]
